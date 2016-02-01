db128c2bbb38dc18a40b69c91e24441eb72cf7f6
Shared <shared@umd-iavs2050l04.local>
2016-02-01 16:55:19 -0500

    basic layout

--- Map_Prototype/ViewController.swift
+++ Map_Prototype/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblLumenilaire: UILabel!
+    @IBOutlet var btnScenes: UIButton!
+    @IBOutlet var btnColorControl: UIButton!
+    @IBOutlet var btnTimerControl: UIButton!
+    @IBOutlet var btnMap: UIButton!
+    @IBOutlet var btnSetup: UIButton!
+    @IBOutlet var lblAddRemove: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
